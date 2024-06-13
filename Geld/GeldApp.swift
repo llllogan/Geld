616a7cca05560fd08c5daa5c5624d57062da4fef
Logan <logan.janssen@icloud.com>
2024-06-13 18:36:44 +1000

    Initial Commit

---
+++ Geld/GeldApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeldApp.swift
+//  Geld
+//
+//  Created by Logan Janssen on 13/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct GeldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GeldTests/GeldTests.swift
@@ -0,0 +1,17 @@
+//
+//  GeldTests.swift
+//  GeldTests
+//
+//  Created by Logan Janssen on 13/6/2024.
+//
+
+import Testing
+@testable import Geld
+
+struct GeldTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
