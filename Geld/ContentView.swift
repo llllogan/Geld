6c2722bffb4a61e8b69cda3f3949dcbac894dfb9
Logan <logan.janssen@icloud.com>
2024-06-13 19:22:04 +1000

    Initial commit. Hello world

--- Geld/ContentView.swift
+++ Geld/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView {
+            
+            VStack {
+                HStack {
+                    Text("$169")
+                        .padding()
+                        .bold()
+                    Text("/ 183")
+                        .font(.subheadline)
+                        .foregroundStyle(.secondary)
+                    Spacer()
+                }
+            }
+            .background(MyMesh())
+            
         }
-        .padding()
+    }
+}
+
+struct MyMesh: View {
+    var body: some View {
+        MeshGradient(
+            width: 3,
+            height: 3,
+            points: [
+                .init(0, 0), .init(0.5, 0), .init(1, 0),
+                .init(0, 0.5), .init(0.3, 0.5), .init(1, 0.5),
+                .init(0, 1), .init(0.5, 1), .init(1, 1)
+            ],
+            colors: [
+                .red, .purple, .indigo,
+                .orange, .cyan, .blue,
+                .yellow, .green, .mint
+            ]
+        )
     }
 }
 
